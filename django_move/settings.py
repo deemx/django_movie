bd24d6ac913c12670143c4b417392fc5d47e5c80
deemx <deemx86@gmail.com>
2021-11-04 22:28:43 +0400

    admin

--- django_move/settings.py
+++ django_move/settings.py
@@ -111,6 +111,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
